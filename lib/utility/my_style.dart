348b8e2bfbc1cc62805cc85774180de839867ec0
masterUNG <phrombutr@gmail.com>
2020-02-12 15:05:32 +0700

    Design UI Authen Success

--- lib/utility/my_style.dart
+++ lib/utility/my_style.dart
@@ -4,7 +4,10 @@ import 'package:flutter/material.dart';
 class MyStyle {
 
   // Field
-  Color textColor = Colors.purple.shade700;
+  
+  Color mainColor = Color.fromARGB(0xff, 0xc0, 0xca, 0x33);
+  Color textColor = Color.fromARGB(0xff, 0x8c, 0x99, 0x00);
+  Color barColor = Color.fromARGB(0xff, 0xf5, 0xfd, 0x67);
 
 
   // Method
--- lib/widget/authen.dart
+++ lib/widget/authen.dart
@@ -10,6 +10,35 @@ class _AuthenState extends State<Authen> {
   // Field
 
   // Method
+  Widget userForm() {
+    return Container(
+      width: 250.0,
+      child: TextField(
+        decoration: InputDecoration(
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: MyStyle().textColor),
+          ),
+          hintStyle: TextStyle(color: MyStyle().textColor),
+          hintText: 'User :',
+        ),
+      ),
+    );
+  }
+
+  Widget passwordForm() {
+    return Container(
+      width: 250.0,
+      child: TextField(
+        decoration: InputDecoration(
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: MyStyle().textColor),
+          ),
+          hintStyle: TextStyle(color: MyStyle().textColor),
+          hintText: 'Password :',
+        ),
+      ),
+    );
+  }
 
   Widget showLogo() {
     return Container(
@@ -30,16 +59,67 @@ class _AuthenState extends State<Authen> {
     );
   }
 
+  Widget signInButton() {
+    return RaisedButton(
+      color: MyStyle().textColor,
+      child: Text(
+        'Sign In',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget singUpButton() {
+    return OutlineButton(
+      borderSide: BorderSide(color: MyStyle().textColor),
+      child: Text(
+        'Sign Up',
+        style: TextStyle(color: MyStyle().textColor),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget mySizeBox() {
+    return SizedBox(
+      width: 5.0,
+      height: 10.0,
+    );
+  }
+
+  Widget showButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        signInButton(),
+        mySizeBox(),
+        singUpButton(),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            showLogo(),
-            showAppName(),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            colors: <Color>[Colors.white, MyStyle().mainColor],radius: 1.0,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              showLogo(),
+              showAppName(),
+              userForm(),
+              passwordForm(),
+              mySizeBox(),
+              showButton(),
+            ],
+          ),
         ),
       ),
     );
